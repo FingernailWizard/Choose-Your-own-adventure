15831b96103b8b56b57436c6cd7ea820fed3b850
Maher <02James.MAher@gulllakecs.org>
2024-03-18 14:20:05 -0400

    start to game

---
+++ Choose own adventure game.py
@@ -0,0 +1,10 @@
+print("You find yourself in a giant human size cage made from bronze and metal. As this cage hangs from a chain bolted to the celing of a rocky cavern, an army of \n"
+"skeletons at the floor bow to a Lich sitting atop it's Golden Throne. Surronding you is other cages holding varios creatures, all looking in terror of the horrors below. \n" 
+"you have no memory of your exsistance and or any one elnce. All you can recall is your skill in combat, even though you can't remember ever learning how. In the cage there is a door. \n"
+"One of the bars surrounding the cage making it unable to leave looks damaged. A bigger cage swings back and forth, in that cage you see a ogre raging in furry swinging it's weight back \n"
+"and forth. You see a rock on the floor of your cage. You pick it up and see a two headed seagull fly by your cage, it seems like a guard and is flying by all the cages.\n")
+
+game = True
+while game:
+    Act1Choice = input("What do you do?\n"
+    "1.Go to the door 2.Check the bar 3.Shift your attention to the ogre ")
