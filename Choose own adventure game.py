ff5b7826028845d15c1556e6f586ed510545c58b
Maher <02James.MAher@gulllakecs.org>
2024-03-19 14:19:34 -0400

    act1story;lng

--- Choose own adventure game.py
+++ Choose own adventure game.py
@@ -2,9 +2,18 @@ print("You find yourself in a giant human size cage made from bronze and metal. 
 "skeletons at the floor bow to a Lich sitting atop it's Golden Throne. Surronding you is other cages holding varios creatures, all looking in terror of the horrors below. \n" 
 "you have no memory of your exsistance and or any one elnce. All you can recall is your skill in combat, even though you can't remember ever learning how. In the cage there is a door. \n"
 "One of the bars surrounding the cage making it unable to leave looks damaged. A bigger cage swings back and forth, in that cage you see a ogre raging in furry swinging it's weight back \n"
-"and forth. You see a rock on the floor of your cage. You pick it up and see a two headed seagull fly by your cage, it seems like a guard and is flying by all the cages.\n")
+"and forth. You see a rock on the floor of your cage. You pick it up and see a two headed human size seagull fly by your cage, it seems like a guard and is flying by all the cages.\n")
 
 game = True
 while game:
     Act1Choice = input("What do you do?\n"
-    "1.Go to the door 2.Check the bar 3.Shift your attention to the ogre ")
+    "1.Go to the door 2.Check the bar 3.Shift your attention to the ogre 4. peer your eye on the seagull 5.do nothing.\n"
+    "Choose from (1, 2, 3, 4, 5): ")
+    print(Act1Choice)
+    if Act1Choice == "1":
+        print("*Act 1 Story Chosen* 'A Doorway Out'")
+    elif Act1Choice == "2":
+        print("*Act 1 Story Chosen* 'Metal Bonds")
+    elif Act1Choice == "3":
+        print("*Act 1 Story Chosen* 'Man & monster'")
+    
